315c26bf1b6e890593b802f637718ab7122e056a
sanin-mn <msanin7003@gmail.com>
2023-10-25 09:35:10 +0530

    pages created and routers added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import PFHome from './Pages/PFHome';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Dashboard from './Pages/Dashboard';
+import Projects from './Pages/Projects';
 
 function App() {
   return (
     <div>
-      hai
+      <Routes>
+        <Route path='/' element={<PFHome/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/projects' element={<Projects/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/PFHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PFHome() {
+  return (
+    <div>PFHome</div>
+  )
+}
+
+export default PFHome
